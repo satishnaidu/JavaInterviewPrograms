841471438ad78b11a71e8a49298afc751f57aba3
Satish Kumar Andey -X (satandey - COGNIZANT TECHNOLOGY SOLUTIONS INDIA PVT LTD at Cisco) <satandey@cisco.com>
2016-06-08 18:48:31 +0530

    Multithreading print abcabc program added

---
+++ src/main/java/com/sample/threads/PrintABC.java
@@ -0,0 +1,47 @@
+package com.sample.threads;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class PrintABC {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
+		List<Callable<String>> callables = new ArrayList<>();
+		Callable<String> a = new MyThread2("A");
+		Callable<String> b = new MyThread2("B");
+		Callable<String> c = new MyThread2("C");
+
+		callables.add(a);
+		callables.add(b);
+		callables.add(c);
+
+		List<Future<String>> results = executorService.invokeAll(callables);
+		for (Future<String> result : results) {
+			System.out.println(result.get().toString());
+		}
+
+		executorService.shutdown();	
+	}
+}
+
+class MyThread2 implements Callable<String> {
+	private String name;
+
+	public MyThread2(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String call() throws Exception {
+		System.out.println(Thread.currentThread().getName());
+		return name;
+	}
+
+}
